aa525a9e3aa7672e77db2014fceebc8a95e33097
ludgmors <ludgmors123@gmail.com>
2025-03-22 02:39:26 -0600

    Descripción clara de los cambios que realizaste

--- frontend/src/pages/Informacion.jsx
+++ frontend/src/pages/Informacion.jsx
@@ -36,7 +36,10 @@ function Informacion() {
     <div className="info-container">
       <h2>Información sobre la participación</h2>
       <p>
-        Aquí va la información sobre el evento o actividad. Por favor, elige si deseas participar.
+      Participa en una actividad deportiva llena de adrenalina y competencia. Disfruta de un torneo donde los equipos lucharán por la victoria, demostrando su habilidad, trabajo en equipo y espíritu competitivo. Es una excelente oportunidad para desafiar tus límites, convivir con otros y poner en práctica tu pasión por el fútbol. ¡Prepárate para una experiencia intensa y llena de emoción!
+      no seas un aburrido asi que acompañanos 
+
+
       </p>
       <div className="botones">
         <button className="btn" onClick={() => handleRespuesta("si")}>Sí</button>
